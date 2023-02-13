8bfef6f07ce1cd76779e7b11cd184ab6a3dbf88b
cneker <staskotashevich@gmail.com>
2023-02-13 12:06:03 +0300

    added shared and home views; added home controller

--- InnoGotchi.MVC/InnoGotchi.MVC/Controllers/HomeController.cs
+++ InnoGotchi.MVC/InnoGotchi.MVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace InnoGotchi.MVC.Controllers
             return View();
         }
 
+        public IActionResult AllInnoGotchi()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
