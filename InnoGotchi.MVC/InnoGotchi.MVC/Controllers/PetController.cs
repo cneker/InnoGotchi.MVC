1ab972aff4f451ba3cb5d0ee3eeea5e640c94b6c
cneker <staskotashevich@gmail.com>
2023-02-17 20:06:08 +0300

    updated PetDetails view and PetController

--- InnoGotchi.MVC/InnoGotchi.MVC/Controllers/PetController.cs
+++ InnoGotchi.MVC/InnoGotchi.MVC/Controllers/PetController.cs
@@ -1,14 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InnoGotchi.MVC.Contracts.Services;
+using InnoGotchi.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InnoGotchi.MVC.Controllers
 {
     public class PetController : Controller
     {
-        public IActionResult PetDetails(Guid petId)
+        private readonly IPetService _petService;
+
+        public PetController(IPetService petService)
         {
+            _petService = petService;
+        }
 
+        public async Task<IActionResult> PetDetails(Guid farmId, Guid petId)
+        {
+            var jwt = Request.Cookies["jwt"];
+            var userId = User.Claims.First(c => c.Type == "Id").Value;
 
-            return View();
+            var pet = await _petService.GetPetDetailsAsync(jwt, userId, farmId.ToString(), petId.ToString());
+
+            return View(new PetDetailsViewModel { PetDetails = pet, FarmId = farmId });
+        }
+
+        public async Task<IActionResult> Feed(Guid farmId, Guid petId)
+        {
+            var jwt = Request.Cookies["jwt"];
+            var userId = User.Claims.First(c => c.Type == "Id").Value;
+
+            await _petService.FeedAsync(jwt, userId, farmId.ToString(), petId.ToString());
+
+            return RedirectToAction("PetDetails", new { farmId, petId });
+        }
+
+        public async Task<IActionResult> GiveADrink(Guid farmId, Guid petId)
+        {
+            var jwt = Request.Cookies["jwt"];
+            var userId = User.Claims.First(c => c.Type == "Id").Value;
+
+            await _petService.GiveADrinkAsync(jwt, userId, farmId.ToString(), petId.ToString());
+
+            return RedirectToAction("PetDetails", new { farmId, petId });
         }
     }
 }
---
+++ InnoGotchi.MVC/InnoGotchi.MVC/ViewModels/PetDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using InnoGotchi.MVC.Models.Pet;
+
+namespace InnoGotchi.MVC.ViewModels
+{
+    public class PetDetailsViewModel
+    {
+        public PetDetailsDto PetDetails { get; set; }
+        public Guid FarmId { get; set; }
+    }
+}
